6cd96be27655b0e5f242228fb73deb3cb84e478a
François Kooman <fkooman@tuxed.net>
2019-12-18 21:29:15 +0100

    use template for coverage

--- src/Coverage.php
+++ src/Coverage.php
@@ -2,56 +2,35 @@
 
 namespace fkooman\Put;
 
+use RuntimeException;
+
 class Coverage
 {
     public static function writeReport($outputFile, array $coverageData)
     {
-        $output = '<html><head><title>Coverage</title><style>
-body {
-    font-family: sans-serif;
-}
-
-code {
-    margin-left: 3em;
-}
-
-code.covered {
-    background-color: lightgreen;
-}
-
-code.uncovered {
-    background-color: red;
-}
-
-div.code {
-    background-color: #eee;
-}
-</style>
-</head><body>';
-
-        foreach ($coverageData as $srcFile => $coverageInfo) {
-            $output .= '<h1>'.basename($srcFile).'</h1>';
-            $output .= '<div class="code">';
-            $fp = fopen($srcFile, 'r');
-            $currentLine = 1;
-            while (false !== $line = fgets($fp)) {
-                if (array_key_exists($currentLine, $coverageInfo)) {
-                    if (-1 === $coverageInfo[$currentLine]) {
-                        $output .= $currentLine.' <code class="uncovered">'.str_replace(' ', '&nbsp;', htmlentities(str_replace("\t", '    ', $line))).'</code><br>';
-                    } else {
-                        $output .= $currentLine.' <code class="covered">'.str_replace(' ', '&nbsp;', htmlentities(str_replace("\t", '    ', $line))).'</code><br>';
-                    }
-                } else {
-                    $output .= $currentLine.' <code>'.str_replace(' ', '&nbsp;', htmlentities(str_replace("\t", '    ', $line))).'</code><br>';
-                }
-
-                ++$currentLine;
+        // get the source code from the files analyzed by the code coverage
+        $templateData = [];
+        foreach (array_keys($coverageData) as $srcFile) {
+            if (false === $filePointer = @fopen($srcFile, 'r')) {
+                throw new RuntimeException(sprintf('unable to open "%s"', $srcFile));
             }
-            $output .= '</div>';
+            $srcCode = [];
+            while (false !== $srcLine = fgets($filePointer)) {
+                $srcCode[] = str_replace([' ', "\t"], ['&nbsp;', '&nbsp;&nbsp;&nbsp;&nbsp;'], htmlentities($srcLine));
+            }
+
+            $templateData[$srcFile] = [
+                'srcCode' => $srcCode,
+                'coverageData' => $coverageData[$srcFile],
+            ];
         }
 
-        $output .= '</body></html>';
-
-        file_put_contents($outputFile, $output);
+        ob_start();
+        include __DIR__.'/coverage_template.php';
+        $htmlPage = ob_get_contents();
+        ob_end_clean();
+        if (false === @file_put_contents($outputFile, $htmlPage)) {
+            throw new RuntimeException(sprintf('unable to write to "%s"', $outputFile));
+        }
     }
 }
---
+++ src/coverage_template.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Test Code Coverage</title>
+    <style>
+        body {
+            font-family: sans-serif;
+        }
+
+        table {
+            background-color: #eee;
+        }
+
+        table th {
+            background-color: #fff;
+        }
+
+        table td.covered {
+            background-color: lightgreen;
+        }
+
+        table td.uncovered {
+            background-color: lightcoral;
+        }
+    </style>
+</head>
+<body>
+<?php foreach ($templateData as $srcFile => $srcInfo): ?>
+    <h1><?=$srcFile; ?></h1>
+    <table>
+<?php foreach ($srcInfo['srcCode'] as $srcLineNo => $srcLineText): ?>
+        <tr>
+            <th><?=$srcLineNo + 1; ?></th>
+<?php if (array_key_exists($srcLineNo + 1, $srcInfo['coverageData'])): ?>
+<?php if (1 === $srcInfo['coverageData'][$srcLineNo + 1]): ?>
+            <td class="covered">
+                <code><?=$srcLineText; ?></code>
+            </td>
+<?php else: ?>
+            <td class="uncovered">
+                <code><?=$srcLineText; ?></code>
+            </td>
+<?php endif; ?>
+<?php else: ?>
+            <td>
+                <code><?=$srcLineText; ?></code>
+            </td>
+<?php endif; ?>
+        </tr>
+<?php endforeach; ?>
+    </table>
+<?php endforeach; ?>
+</body>
+</html>
