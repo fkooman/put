9a05a5420fcdaadd61a24354a2e496effb1bb811
François Kooman <fkooman@tuxed.net>
2020-01-19 11:33:49 +0100

    fix test

--- tests/SimpleTest.php
+++ tests/SimpleTest.php
@@ -13,7 +13,7 @@ class SimpleTest extends TestCase
     public function testDate()
     {
         $dateTime = new DateTime('2019-01-01 08:00:00');
-        $this->assertSame('2019-01-02', $dateTime->format('Y-m-d'));
+        $this->assertSame('2019-01-01', $dateTime->format('Y-m-d'));
     }
 
     /**
